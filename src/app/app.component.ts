c21342e49afff239f3f9628d11050e996932f04f
anuj <anujg0046@gmail.com>
2021-09-11 16:45:31 +0530

    code updated

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todo';
+  newTodo: string;
+  todos: any;
+  todoObj: any;
+
+  constructor() {
+    this.newTodo = '';
+    this.todos = [];
+  }
+
+  addTodo(event) {
+    this.todoObj = {
+      newTodo: this.newTodo,
+      completed: false
+    }
+    this.todos.push(this.todoObj);
+    localStorage.setItem('todolist', JSON.stringify(this.todos));
+    this.newTodo = '';
+    event.preventDefault();
+  }
+
+  deleteTodo(index) {
+    this.todos.splice(index, 1);
+  }
+
+}
